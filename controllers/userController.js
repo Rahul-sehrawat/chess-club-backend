a9a9127dfadb40402a958fb3ee766b76b38d1618
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-06-21 09:50:16 +0530

    passing username in json

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,11 +11,12 @@ const loginUser = async (req, res) => {
 
   try {
     const user = await User.login(email, password)
+    const username = user.name; 
 
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token})
+    res.status(200).json({email, token, username})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
