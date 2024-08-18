2929d2e072d2955b73700a68a31b56cdb0e58df5
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-08-18 16:41:01 +0530

    error fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,13 +10,11 @@ const loginUser = async (req, res) => {
   const {email, password} = req.body
 
   try {
-    const user = await User.login(email, password)
-    const username = user.name; 
-
+    const user = await User.login(email, password) 
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token, username})
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
