3efbdc04d4a142869cae8fb5ee3e65e5f2826053
inv-amalusudhakaran <amalu.sudhakaran@innovaturelabs.com>
2024-08-27 15:22:17 +0530

    fixed username error with login credential

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ const loginUser = async (req, res) => {
     // create a token
     const token = createToken(user._id)
 
-    res.status(200).json({email, token})
+    res.status(200).json({email, token,name:user.name})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
